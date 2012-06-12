bfdf274b2c1e98e652f3c82ea0b5a30c8339f551
antofosm <antofosm@gmail.com>
2012-06-12 15:24:52 +0200

    improved edge drawing, added basic text labeling

--- htdocs/scripts/drawing.js
+++ htdocs/scripts/drawing.js
@@ -61,9 +61,21 @@ Vis.Drawing = {
     DrawEdge : function(from, to, weight) {
         var gl = Vis.WebGL.Context;
         
-        var xOffset = 0.01;
-        var yOffset = 0.01;  //should depend on weight AND direction
-
+        //in order to get the polygon vertices, whose positions depend on the authors' positions
+        //as well as edge thickness, find a vector orthogonal to the edge, normalize it, and 
+        //multiply by weight
+		var xOrtho = -(to.y - from.y);
+        var yOrtho = (to.x - from.x);
+        var len = Math.sqrt(Math.pow(xOrtho, 2) + Math.pow(yOrtho, 2));
+        var xOffset = 0.01 * weight * xOrtho/len;
+        var yOffset = 0.01 * weight * yOrtho/len/this.s;
+        
+        //then limit the width
+        xOffset = xOffset < 0.005 ? 0 : xOffset;
+        xOffset = xOffset > 0.1 ? 0.1 : xOffset;
+        yOffset = yOffset < 0.005 ? 0 : yOffset;
+        yOffset = yOffset > 0.1 ? 0.1 : yOffset;
+        
 		var vertices = [
 		    from.x - xOffset, from.y - yOffset,
 		    from.x + xOffset, from.y + yOffset,
@@ -74,8 +86,9 @@ Vis.Drawing = {
         gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
         gl.vertexAttribPointer(Vis.WebGL.Shaders.BasicShader.vertexPositionAttribute, 2, gl.FLOAT, false, 0, 0);
-
-		var lightgrey = 0.5 + weight;
+        
+        //define the color scale, again depending on the weight
+		var lightgrey = 0.5 + 0.4*weight;       //this is arbitrary, normalized weights would be better....
 		lightgrey = lightgrey > 1 ? 1 : lightgrey;
 		
 		var colors = [
@@ -92,10 +105,47 @@ Vis.Drawing = {
         gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
     },
     //******************************************************************************************
+    //* Draws text (using DIVs)
+    //******************************************************************************************
+    DrawText : function(localpos, text) {
+        var textdiv = document.createElement('div');
+        textdiv.setAttribute('id', 'text_' + text);
+        
+        var width = text.length * 7;    //could be determined from the font size as well...
+        var height = 15;
+        var xExtent = 1;
+        var yExtent = this.Height/2;
+        var left = Vis.WebGL.Canvas.Left + xExtent * Vis.WebGL.Canvas.Width/2 * (1 + localpos.x) - width/2;
+        var top = Vis.WebGL.Canvas.Top + yExtent * Vis.WebGL.Canvas.Height/2 * (1 + localpos.y) - height/2;
+        
+        textdiv.style.position = "absolute";
+        textdiv.style.width = width + 'px';
+        textdiv.style.height = height + 'px';
+        textdiv.style.left = left + 'px';
+        textdiv.style.top = top + 'px';
+        
+        textdiv.innerHTML = text;
+        
+        document.body.appendChild(textdiv);
+    },
+    //******************************************************************************************
+    //* Clears text
+    //******************************************************************************************
+    ClearText : function() {
+        var divs = document.getElementsByTagName("div");
+        for (var n = 0; n < 10; n++) {      //strange, but works better
+            for (var i = 0; i < divs.length; i++) {
+                if (divs[i].id.indexOf('text_') == 0) {
+                    document.body.removeChild(divs[i]);
+                }
+            }
+        }
+    },
+    //******************************************************************************************
     //* Draws the main ellipsis
     //******************************************************************************************
     DrawMainEllipsis : function() {
-        this.DrawEllipsis({x: 0, y: 0}, this.scale, this.s, 48, [1.0, 0.0, 0.0, 0.4]);
+        this.DrawEllipsis({x: 0, y: 0}, this.scale, this.s, 48, [1.0, 1.0, 1.0, 1.0]);
     },
     //******************************************************************************************
     //* Draws the authors as ellipses
@@ -107,8 +157,8 @@ Vis.Drawing = {
 		    if ((inv = Math.sqrt(Math.pow(this.positions[i].p1, 2) + Math.pow(this.positions[i].p2, 2))) != 0) {
 		        x = this.scale * this.positions[i].p1 / inv;
                 y = this.s * this.scale * this.positions[i].p2 / inv;
-                
-		        ratio = this.positions[i].out / (this.positions[i].in + 0.01);    //just to make sure we don't divide by zero
+		        
+		        ratio = this.positions[i].out / (this.positions[i].in + 0.001);    //just to make sure we don't divide by zero
 		        ratio = ratio < 0.333 ? 0.333 : ratio;  //limit the ratio
 		        ratio = ratio > 3 ? 3 : ratio;
 		        
@@ -118,12 +168,17 @@ Vis.Drawing = {
 
 		            =>  (PI*a*a*ratio) / inv = const.
 		            =>  a² = const. * inv / (PI * ratio)    */
-		        
-		        a = Math.sqrt(0.05 * inv / (Math.PI * ratio));  //the constant may require some testing
-		        
-                this.DrawEllipsis({x: x, y: y}, a, ratio, 48, [0.0, 0.8, 0.4, 1.0]);
+
+		        a = Math.sqrt(0.05 * inv / (Math.PI * ratio));  //the constant defines the average size of
+		                                                        //the authors' ellipses
                 
-                this.AuthorsXY[this.positions[i].name] = {x: x, y: y};
+		        //draw ellipses - but not the invisibly small ones
+		        if (a > 1e-3) {
+                    this.DrawEllipsis({x: x, y: y}, a, ratio, 24, [0.0, 0.8, 0.4, 1.0]);
+                    this.DrawText({x: x, y: y}, this.positions[i].name);
+                    //store XY
+                    this.AuthorsXY[this.positions[i].name] = {x: x, y: y};
+                }
             }
 		}
     },
@@ -137,10 +192,11 @@ Vis.Drawing = {
                 
                 for (var target in this.positions[i].revised) {
                     if (this.AuthorsXY[target]) {
+                        //alert(this.positions[i].name+", "+target);
                         this.DrawEdge(
-                                from,
-                                this.AuthorsXY[target],
-                                this.positions[i].revised[target]
+                            from,
+                            this.AuthorsXY[target],
+                            this.positions[i].revised[target]
                         );
                     }
                 }
@@ -166,7 +222,8 @@ Vis.Drawing = {
 
 		shaderProgram.vertexColorAttribute = gl.getAttribLocation(shaderProgram, "aVertexColor");
 		gl.enableVertexAttribArray(shaderProgram.vertexColorAttribute);
-
+		
+        this.ClearText();
         this.DrawAuthors();
         this.DrawRevisionEdges();
         this.DrawMainEllipsis();
